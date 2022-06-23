4e5c7c53b0ee5db49d93ee6d1b9c2bbd5dffe923
AnkitSingh60 <ankitanwar730@gmail.com>
2022-06-23 23:06:34 +0530

    added some css in basic structure

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 
 const Weather = () => {
   return (
-    <div>
-        <div>
-            <div>
+    <div className="mainDiv">
+        <div className="div1">
                 <input type="text" placeholder="Search"  />
                 <h1>Delhi</h1>
                 <h4 className='temp'>26°C</h4>  
-            </div>
         </div>
     </div>
   )
