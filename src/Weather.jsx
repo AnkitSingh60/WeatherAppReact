bd541664a25c6b1e55727940d2dc72103071c26e
AnkitSingh60 <ankitanwar730@gmail.com>
2022-06-24 14:06:42 +0530

    added useState hook

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Weather = () => {
+  const [city, setCity] = useState(null)
+  const [search, setSearch] = useState("")
   return (
     <div className="mainDiv">
         <div className="div1">
-                <input type="text" placeholder="Search"  />
+                <input type="search" onChange={()=>{}} placeholder="Search"  />
                 <h1>Delhi</h1>
                 <h4 className='temp'>26°C</h4>  
         </div>
